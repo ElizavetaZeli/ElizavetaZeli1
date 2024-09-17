3e153912ab725a5db86a289014e3b148f149beed
ElizavetaZeli <kelli.ellison@mail.ru>
2024-09-17 17:14:41 +0300

    Переменные

---
+++ main.py
@@ -0,0 +1,7 @@
+homework_completed_count = 12
+hours_count = 1.5
+name = 'Python'
+print(1.5 / 12)
+time_for_one_task = 0.125
+print('Курс:',name,',' 'всего задач:',homework_completed_count,',''затрачено часов:',hours_count,','
+'среднее время выполнения:',time_for_one_task)
